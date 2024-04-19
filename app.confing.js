94678ece2d66fcec0294999f1a10e502f8f181bf
wabrodom <113647599+wabrodom@users.noreply.github.com>
2024-04-19 15:14:25 +0700

    make sure .env is not push to remove repo

--- app.confing.js
+++ app.confing.js
@@ -1,6 +1,10 @@
-{
-  "expo": {
+
+export default {
     "name": "rate-repository-app",
+    extra: {
+      env: process.env.ENV,
+      APOLLO_URI: process.env.APOLLO_URI,
+    }, 
     "slug": "rate-repository-app",
     "version": "1.0.0",
     "orientation": "portrait",
@@ -27,4 +31,8 @@
       "favicon": "./assets/favicon.png"
     }
   }
-}
+
+
+
+// app.json -> app.config.js
+// remove {} on json format, remove expo key and export that object
